71641a4fc7df4781a3cdd2f8a6e8a765d070a46c
JoseGarcia <garciarubiojose79@gmail.com>
2023-09-14 11:37:33 -0600

    Creando rutas de login/registrar

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    //
+    public function login(Request $request){
+
+        return response()->json("Implementar metodo de login");
+    }
+
+    public function register (Request $request){
+        return response()->json("Implementar metodo de registrar");
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::group(['prefix' => 'auth'], function (){
+    Route::post('registrar', [UsuarioController::class, 'register'])->name('registrar');
+    Route::post('login', [UsuarioController::class, 'login'])->name('login');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
