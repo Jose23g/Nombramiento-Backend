fd89b1eb45e9e932a596ce9bb77cce452b735a07
Jose Alberto Guevara Morales <5410jos@gmail.com>
2023-09-15 16:09:15 -0600

    feat(controllers): modify controllers to allow searching by id feat(routes): add API routes feat(tests): verify the functionality of the APIs

--- app/Http/Controllers/ProvinciaController.php
+++ app/Http/Controllers/ProvinciaController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Provincia;
 
 class ProvinciaController extends Controller
 {
-    //
+    public function obtengaLaLista()
+    {
+        return Provincia::all()->toJson();
+    }
+    public function obtengaPorId(Request $request)
+    {
+        if ($request->has('id')) {
+            return Provincia::find($request->get('id'))->toJson();
+        }
+        return response()->json('error: No se han enviado los datos solicitados', 441);
+    }
 }
