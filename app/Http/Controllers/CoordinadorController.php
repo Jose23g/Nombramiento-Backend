ff3995d7cc7a805cbf9a72b63635fd3ded9f07c9
Josue Machado Velasquez <66174344+JosueM3615@users.noreply.github.com>
2023-09-28 18:46:26 -0600

    Controlador Coordinador

---
+++ app/Http/Controllers/CoordinadorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoordinadorController extends Controller
+{
+    
+    public function Solicitud_de_curso(Request $request){
+
+    }
+
+    public function Ver_Solicitud_curso(Request $request){
+
+    }
+
+    public function Editar_Solicitud_curso(Request $request){
+        
+    }
+
+    public function Ver_Estado_Solicitud(Request $request){
+        
+    }
+}
