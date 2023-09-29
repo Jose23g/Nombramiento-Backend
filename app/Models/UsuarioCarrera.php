ce840f298927212c422993c93fd079ce36a7bc10
Jose Alberto Guevara Morales <5410jos@gmail.com>
2023-09-28 22:53:03 -0600

    Se hacen modificaciones en los modelos para realizar las relaciones

--- app/Models/UsuarioCarrera.php
+++ app/Models/UsuarioCarrera.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class UsuarioCarrera extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    protected $table = 'usuario_carreras';
+
+    public function coordinador()
+    {
+        return $this->belongsTo(Usuario::class, 'id_coordinador');
+    }
+
+    public function carrera()
+    {
+        return $this->belongsTo(Carrera::class, 'id_carrera');
+    }
+
 }
