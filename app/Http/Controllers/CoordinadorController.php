2dd8d70dcfc453fa4b7f9e1d25309bb152c34896
Jose Garcia <garciarubiojose79@gmail.com>
2023-10-24 17:34:25 -0600

    resolviendo conflictos

--- app/Http/Controllers/CoordinadorController.php
+++ app/Http/Controllers/CoordinadorController.php
@@ -193,6 +193,6 @@ class CoordinadorController extends Controller
             return $solicitudRefactorized;
         });
 
-        return response()->json($solicitudesCursoRefactorized, 201);
+        return response()->json($solicitudesCursoRefactorized, 200);
     }
 }
